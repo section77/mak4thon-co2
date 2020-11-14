e3c165caf568b3cf9d885bc992a3ac68d64272a6
Justin Otherguy <justin@justinotherguy.org>
2020-11-14 15:20:39 +0100

    first display integration

--- CO2-api/CO2-api.ino
+++ CO2-api/CO2-api.ino
@@ -24,35 +24,85 @@
  *
  */
 
+/*
+
+  GraphicsTest.ino
+
+  Some graphics/text output for U8x8 API
+
+  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
+
+  Copyright (c) 2016, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list
+    of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice, this
+    list of conditions and the following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+#include <Arduino.h>
+#include <U8x8lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
 
 #include <Wire.h>
 #include <Arduino.h>
+
 #include <WiFi.h>
 #include <WiFiMulti.h>
+
 #include <HTTPClient.h>
-
 #include "wlan_settings.h"
-#include "uuids.h"
-#include "SparkFun_SCD30_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_SCD30
 
+#include "uuids.h"
 
 const int ledPinRed = 17; 
 const int ledPinGreen = 12; 
 const int ledPinBlue = 13; 
 
-
+#include "SparkFun_SCD30_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_SCD30
 SCD30 airSensor;
+
 WiFiMulti wifiMulti;
 
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);         // FIXME Hiltec...
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.println("SCD30 Example");
   Wire.begin();
 
-  if (airSensor.begin() == false) {
+  if (airSensor.begin() == false)
+  {
     Serial.println("Air sensor not detected. Please check wiring. Freezing...");
-    while (1){};
+    while (1)
+      ;
   }
 
   wifiMulti.addAP(ssid, wlan_password);
@@ -66,9 +116,49 @@ void setup() {
   pinMode(ledPinRed, OUTPUT);
   pinMode(ledPinGreen, OUTPUT);
   pinMode(ledPinBlue, OUTPUT);
+
+  u8x8.begin();
+
 }
 
-void push_volkszaehler(const char* UUID, float value) {
+void pre(void) // part of GraphicsTest
+{
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
+  u8x8.clear();
+
+  u8x8.inverse();
+  u8x8.print(" U8x8 Library ");
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.noInverse();
+  u8x8.setCursor(0,1);
+}
+
+void draw_bar(uint8_t c, uint8_t is_inverse)
+{
+  uint8_t r;
+  u8x8.setInverseFont(is_inverse);
+  for( r = 0; r < u8x8.getRows(); r++ )
+  {
+    u8x8.setCursor(c, r);
+    u8x8.print(" ");
+  }
+}
+
+void draw_ascii_row(uint8_t r, int start)
+{
+  int a;
+  uint8_t c;
+  for( c = 0; c < u8x8.getCols(); c++ )
+  {
+    u8x8.setCursor(c,r);
+    a = start + c;
+    if ( a <= 255 )
+      u8x8.write(a);
+  }
+}
+
+void push_volkszaehler (const char* UUID, float value)
+{
   HTTPClient http;
 
   //Serial.print("[HTTP] begin...\n");
@@ -82,24 +172,30 @@ void push_volkszaehler(const char* UUID, float value) {
   int httpCode = http.GET();
 
   // httpCode will be negative on error
-  if (httpCode > 0) {
-      // HTTP header has been send and Server response header has been handled
-      //Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-      // file found at server
-      if (httpCode == HTTP_CODE_OK) {
-        String payload = http.getString();
-        //Serial.println(payload);
-      }
-    } else {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  if(httpCode > 0)
+    {
+        // HTTP header has been send and Server response header has been handled
+        //Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+  
+        // file found at server
+        if(httpCode == HTTP_CODE_OK) {
+            String payload = http.getString();
+            //Serial.println(payload);
+        }
+    }
+  else
+    {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
   http.end();
+
 }
 
-void loop() {
-  if (airSensor.dataAvailable()) {
+void loop()
+{
+  if (airSensor.dataAvailable())
+  {
     Serial.print("co2(ppm):");
     Serial.print(airSensor.getCO2());
 
@@ -112,20 +208,30 @@ void loop() {
     Serial.println();
 
     // wait for WiFi connection
-    if ((wifiMulti.run() == WL_CONNECTED)) {
+    if((wifiMulti.run() == WL_CONNECTED))
+    {
       push_volkszaehler (uuid_co2, airSensor.getCO2());
       push_volkszaehler (uuid_temp, airSensor.getTemperature());
       push_volkszaehler (uuid_humidity, airSensor.getHumidity());
     }
-  } else {
-    Serial.println("Waiting for new data");
   }
+  else
+    Serial.println("Waiting for new data");
 
-  if (airSensor.getCO2() > 1000) { // red
+  pre();
+  u8x8.print("CO2:");
+  u8x8.setCursor(0,2);
+  u8x8.print(airSensor.getCO2());
+
+//  u8g2.firstPage();
+//  u8g2.setFont(u8g2_font_ncenB24_tr);
+//  u8g2.drawStr(0,24,"Hello World!");
+  
+  if(airSensor.getCO2() > 1000) { // red
       digitalWrite(ledPinRed, HIGH);    
       digitalWrite(ledPinGreen, LOW);
       digitalWrite(ledPinBlue, LOW);
-  } else if (airSensor.getCO2() > 800) {  // yellow
+  } else if(airSensor.getCO2() > 800) {  // yellow
       digitalWrite(ledPinRed, HIGH);    
       digitalWrite(ledPinGreen, HIGH);
       digitalWrite(ledPinBlue, LOW);
@@ -136,4 +242,5 @@ void loop() {
   }
 
   delay(1500);
+
 }

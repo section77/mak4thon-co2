2894a684e6112134f07ee47641fd1c577dcf1f6b
Andreas Weber <andy@josoansi.de>
2020-11-18 14:18:28 +0100

    Removed redundant upload files

--- sciebo_upload/Visualisierung/poll-volkszaehler/php/config/poll_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return $poll_config = [
-    'influxdb_host' => 'influxdb',
-    'influxdb_port' => '8086',
-    'poll_interval' => 30,
-    'poll_sleep_between_requests' => 2,
-    'verbose' => FALSE,
-    'delaying_start_seconds' => 10,
-];
